0c287b5361dda8b13062b750e9a0428d53914480
edewey50-prog <edewey50@gmail.com>
2026-09-15 15:21:15 -0600

    Update check_updates.py

--- check_updates.py
+++ check_updates.py
@@ -78,9 +78,12 @@ def send_to_discord(update):
     if not WEBHOOK_URL:
         raise RuntimeError("DISCORD_WEBHOOK_URL is not configured.")
 
-    payload = {
-        "content": "➶ **NEW STAR STABLE GAME UPDATE!**",
-        "embeds": [
+ payload = {
+    "content": "<@&1549507550377156700> ➶ **NEW STAR STABLE GAME UPDATE!**",
+    "allowed_mentions": {
+        "roles": ["1549507550377156700"]
+    },
+    "embeds": [
             {
                 "title": update["title"],
                 "url": update["url"],
